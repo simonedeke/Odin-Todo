ce15751d8329457e4e4377fa0f07b17e30df09d0
Simon Deke <simonedeke@gmail.com>
2022-10-23 02:14:00 -0500

    start modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import Task from './task.js';
 
 
 console.log("hello");
--- src/task.js
+++ src/task.js
@@ -1,1 +1,22 @@
-conole.log("hello");
+
+export function tdItemManager() {
+
+
+
+    function createToDo(title, description, dueDate, priority, project, complete) {
+        return {
+            title,
+            description,
+            dueDate,
+            priority,
+            project,
+            complete
+        }
+    }
+
+    function editToDo() {
+
+    }
+
+
+}
